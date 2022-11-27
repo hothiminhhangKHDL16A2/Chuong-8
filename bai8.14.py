6fa412cb8a62eb106fc8ba1522cb8f6a35d6b08f
hothiminhhang0024 <117257079+hothiminhhangKHDL16A2@users.noreply.github.com>
2022-11-27 11:04:18 +0700

    Add files via upload

---
+++ bai8.14.py
@@ -0,0 +1,7 @@
+print("Nhập n:")
+n=eval(input(""))
+a = int(input("Nhập số nguyên thứ 1:"))
+b = int(input("Nhập số nguyên thứ 2:"))
+c = int (input("Nhập số nguyên thứ 3:"))
+S = a+b+c
+print("Tổng S:", S)
